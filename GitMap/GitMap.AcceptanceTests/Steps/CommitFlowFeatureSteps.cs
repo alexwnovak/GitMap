00df0c3dc48767a1fe270a3ce1fe520bcc31e387
Alex Novak <alexwnovak@gmail.com>
2017-11-26 10:57:09 -0500

    +Pending commit flow acceptance test

---
+++ GitMap/GitMap.AcceptanceTests/Steps/CommitFlowFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GitMap.AcceptanceTests.Steps
+{
+   [Binding]
+   public class CommitFlowFeatureSteps
+   {
+      [Given( @"I have a valid COMMIT_EDITMSG file" )]
+      public void GivenIHaveAValidCOMMIT_EDITMSGFile()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Given( @"my commit editor is configured to be notepad\.exe" )]
+      public void GivenMyCommitEditorIsConfiguredToBeNotepad_Exe()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When( @"the application launches" )]
+      public void WhenTheApplicationLaunches()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then( @"my configured commit editor is launched with the commit file" )]
+      public void ThenMyConfiguredCommitEditorIsLaunchedWithTheCommitFile()
+      {
+         ScenarioContext.Current.Pending();
+      }
+   }
+}
