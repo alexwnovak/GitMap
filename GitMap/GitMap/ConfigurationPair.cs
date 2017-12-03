2c8a4cbf34def36a93f5293bc4b734ff024b54e8
Alex Novak <alexwnovak@gmail.com>
2017-12-03 14:56:07 -0500

    +ConfigurationPair for file path and arguments
    This is what will be stored for configuration, and we'll read it back out. It should be unprocessed and whoever will have to munge it to make it work with the incoming file.

---
+++ GitMap/GitMap/ConfigurationPair.cs
@@ -0,0 +1,21 @@
+﻿namespace GitMap
+{
+   public class ConfigurationPair
+   {
+      public string FilePath
+      {
+         get;
+      }
+
+      public string Arguments
+      {
+         get;
+      }
+
+      public ConfigurationPair( string filePath, string arguments )
+      {
+         FilePath = filePath;
+         Arguments = arguments;
+      }
+   }
+}
