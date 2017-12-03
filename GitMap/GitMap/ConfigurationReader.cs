474db11c16dff098d725e6e56b77b2d5bd84af87
Alex Novak <alexwnovak@gmail.com>
2017-12-03 15:47:00 -0500

    Hard-coding notepad++ x64 as the commit editor for now

--- GitMap/GitMap/ConfigurationReader.cs
+++ GitMap/GitMap/ConfigurationReader.cs
@@ -1,10 +1,20 @@
-﻿namespace GitMap
+﻿using System;
+
+namespace GitMap
 {
    public class ConfigurationReader : IConfigurationReader
    {
       public ConfigurationPair Read<T>()
       {
-         throw new System.NotImplementedException();
+         if ( typeof( T ) == typeof( CommitWorkflow ) )
+         {
+            string filePath = @"C:\Program Files\Notepad++\notepad++.exe";
+            string arguments = @"-multiInst -notabbar -nosession -noPlugin %1";
+
+            return new ConfigurationPair( filePath, arguments );
+         }
+
+         throw new NotImplementedException();
       }
    }
 }
