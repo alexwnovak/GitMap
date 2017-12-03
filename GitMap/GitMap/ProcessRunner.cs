7058dc20e9838c3edcc2b31db4e3e09a4ca90768
Alex Novak <alexwnovak@gmail.com>
2017-12-03 15:19:09 -0500

    +Process runner implementation

---
+++ GitMap/GitMap/ProcessRunner.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace GitMap
+{
+   public class ProcessRunner : IProcessRunner
+   {
+      public void Run( string fileName, string arguments )
+      {
+         var process = Process.Start( fileName, arguments );
+
+         process.WaitForExit();
+      }
+   }
+}
