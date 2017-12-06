9fa327882c3e7a5b24b87cc5f0367a2ce5815bc1
Alex Novak <alexwnovak@gmail.com>
2017-12-06 12:48:51 -0500

    Condensing the entry point
    - Making it easier to create the workflows
    - Re-using dependencies, since they're probably all the same
    - Short-handing controller run and returning its exit code

--- GitMap/GitMap/Program.cs
+++ GitMap/GitMap/Program.cs
@@ -5,17 +5,20 @@ namespace GitMap
 {
    internal static class Program
    {
+      private static readonly IConfigurationReader _configurationReader = new ConfigurationReader();
+      private static readonly IProcessRunner _processRunner = new ProcessRunner();
+
+      private static IWorkflow CreateWorkflow( string workflowName ) =>
+         new Workflow( workflowName, _configurationReader, _processRunner );
+
       private static int Main( string[] arguments )
       {
-         var commitWorkflow = new Workflow( "CommitWorkflow", new ConfigurationReader(), new ProcessRunner() );
-
          var workflows = new Dictionary<string, IWorkflow>
          {
-            [GitFileNames.CommitFileName] = commitWorkflow
+            [GitFileNames.CommitFileName] = CreateWorkflow( "CommitWorkflow" )
          };
 
-         var appController = new AppController( workflows );
-         return appController.Run( arguments );
+         return new AppController( workflows ).Run( arguments );
       }
    }
 }
