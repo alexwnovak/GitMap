fd13231764ebd8a21c896a2702f5b78f9a118fb9
Alex Novak <alexwnovak@gmail.com>
2017-12-06 13:00:14 -0500

    +AppControllerFactory
    Scooping most of the entry point creation guts into this guy. This way the entry point as well as the acceptance tests can use the factory to generate a ready-to-go AppController that we can just call Run on.

---
+++ GitMap/GitMap/AppControllerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using GitModel;
+
+namespace GitMap
+{
+   public static class AppControllerFactory
+   {
+      private static readonly IConfigurationReader _configurationReader = new ConfigurationReader();
+      private static readonly IProcessRunner _processRunner = new ProcessRunner();
+
+      private static IWorkflow CreateWorkflow( string workflowName ) =>
+         new Workflow( workflowName, _configurationReader, _processRunner );
+
+      public static AppController Create()
+      {
+         var workflows = new Dictionary<string, IWorkflow>
+         {
+            [GitFileNames.CommitFileName] = CreateWorkflow( WorkflowNames.CommitWorkflow )
+         };
+
+         return new AppController( workflows );
+      }
+   }
+}
