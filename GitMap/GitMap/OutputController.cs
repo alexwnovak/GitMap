0c3b177f42e18489403b6ade383897a2d9eec4a6
Alex Novak <alexwnovak@gmail.com>
2017-12-06 21:31:14 -0500

    Implementation for DisplayBanner

--- GitMap/GitMap/OutputController.cs
+++ GitMap/GitMap/OutputController.cs
@@ -1,10 +1,22 @@
-﻿namespace GitMap
+﻿using System;
+using System.Reflection;
+
+namespace GitMap
 {
    public class OutputController : IOutputController
    {
       public void DisplayBanner()
       {
-         
+         var previousColor = Console.ForegroundColor;
+
+         Console.ForegroundColor = ConsoleColor.Cyan;
+
+         var thisAssembly = Assembly.GetExecutingAssembly();
+         var assemblyName = thisAssembly.GetName();
+         string version = $"{assemblyName.Version.Major}.{assemblyName.Version.Minor}.{assemblyName.Version.Build}";
+
+         Console.WriteLine( $"{assemblyName.Name} version {version}");
+         Console.ForegroundColor = previousColor;
       }
    }
 }
