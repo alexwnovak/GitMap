4119dbf3a0a2f7a56cee9cafbe8a94dbbf51ef35
Alex Novak <alexwnovak@gmail.com>
2018-02-16 10:12:25 -0500

    +Blank ConfigurationWorkflow
    This will respond to the empty string, so when there are no parameters it'll resolve to this. This'll turn around and launch the UI, probably in line, not as a separate process.

---
+++ GitMap/GitMap/ConfigurationWorkflow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitMap
+{
+   public class ConfigurationWorkflow : IWorkflow
+   {
+      public int Launch( string parameter )
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
