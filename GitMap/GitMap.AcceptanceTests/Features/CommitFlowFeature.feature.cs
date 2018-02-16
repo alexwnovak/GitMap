b6657d0015a8e0b16402eb2b280335335348da17
Alex Novak <alexwnovak@gmail.com>
2018-02-16 09:44:45 -0500

    Moving features into a new .\Features directory

--- GitMap/GitMap.AcceptanceTests/Features/CommitFlowFeature.feature.cs
+++ GitMap/GitMap.AcceptanceTests/Features/CommitFlowFeature.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace GitMap.AcceptanceTests
+namespace GitMap.AcceptanceTests.Features
 {
     using TechTalk.SpecFlow;
     
