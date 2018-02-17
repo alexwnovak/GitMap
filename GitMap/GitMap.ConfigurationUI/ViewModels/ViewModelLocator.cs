8a468fd1a17981cd7018a9f36342b8284106ea67
Alex Novak <alexwnovak@gmail.com>
2018-02-17 09:24:03 -0500

    +Mvvmlight package

---
+++ GitMap/GitMap.ConfigurationUI/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,32 @@
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace GitMap.ConfigurationUI.ViewModels
+{
+   public class ViewModelLocator
+   {
+      public ViewModelLocator()
+      {
+         ServiceLocator.SetLocatorProvider( () => SimpleIoc.Default );
+
+         ////if (ViewModelBase.IsInDesignModeStatic)
+         ////{
+         ////   // Create design time view services and models
+         ////   SimpleIoc.Default.Register<IDataService, DesignDataService>();
+         ////}
+         ////else
+         ////{
+         ////   // Create run time view services and models
+         ////   SimpleIoc.Default.Register<IDataService, DataService>();
+         ////}
+
+         SimpleIoc.Default.Register<MainViewModel>();
+      }
+
+      public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+      public static void Cleanup()
+      {
+      }
+   }
+}
