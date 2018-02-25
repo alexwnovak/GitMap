b99b30acf0f47c3ff407679146e9e73e42b7621f
Alex Novak <alexwnovak@gmail.com>
2018-02-24 22:39:34 -0500

    Registering the editor view models
    These will be delivered via constructor injection to MainViewModel on launch, who stores them and has XAML bindings to display them. These view models power the tabs that are displayed, and the data they present.

--- GitMap/GitMap.ConfigurationUI/App.xaml.cs
+++ GitMap/GitMap.ConfigurationUI/App.xaml.cs
@@ -1,8 +1,22 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using GitMap.ConfigurationUI.ViewModels;
+using Resx = GitMap.ConfigurationUI.Properties.Resources;
 
 namespace GitMap.ConfigurationUI
 {
    public partial class App : Application
    {
+      protected override void OnStartup( StartupEventArgs e )
+      {
+         var editorViewModels = new []
+         {
+            new EditorViewModel { Header = Resx.Commit, IsEnabled = true },
+            new EditorViewModel { Header = Resx.Rebase, IsEnabled = false }
+         };
+
+         SimpleIoc.Default.Register<IEnumerable<EditorViewModel>>( () => editorViewModels );
+      }
    }
 }
