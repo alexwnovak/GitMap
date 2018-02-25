26d383fa483afbb0a0282776e8d8a7f900a745e7
Alex Novak <alexwnovak@gmail.com>
2018-02-24 22:37:37 -0500

    +EditorViewModel stub
    This is the data for the UI representation. It houses the name of the editor (for the tab text), whether it's an enabled editor, and eventually, the actual data for the editor and its arguments.

---
+++ GitMap/GitMap.ConfigurationUI/ViewModels/EditorViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace GitMap.ConfigurationUI.ViewModels
+{
+   public class EditorViewModel : ViewModelBase
+   {
+      private string _header;
+      public string Header
+      {
+         get => _header;
+         set => Set( nameof( Header ), ref _header, value );
+      }
+
+      private bool _isEnabled;
+      public bool IsEnabled
+      {
+         get => _isEnabled;
+         set => Set( nameof( IsEnabled ), ref _isEnabled, value );
+      }
+   }
+}
