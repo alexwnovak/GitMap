7452edbea1b5d81747d71e9bb9960b717f397371
Alex Novak <alexwnovak@gmail.com>
2018-02-28 20:35:16 -0500

    Fleshing out the open file dialog call

--- GitMap/GitMap.ConfigurationUI/Services/FileBrowserService.cs
+++ GitMap/GitMap.ConfigurationUI/Services/FileBrowserService.cs
@@ -1,10 +1,20 @@
-﻿namespace GitMap.ConfigurationUI.Services
+﻿using Microsoft.Win32;
+
+namespace GitMap.ConfigurationUI.Services
 {
    public class FileBrowserService : IFileBrowserService
    {
       public string PickSingleFile()
       {
-         throw new System.NotImplementedException();
+         var openFileDialog = new OpenFileDialog
+         {
+            Filter = "Executable files|*.exe;*.bat",
+            Title = "Choose Editor"
+         };
+         
+         var result = openFileDialog.ShowDialog();
+
+         return result.HasValue && result.Value ? openFileDialog.FileName : null;
       }
    }
 }
