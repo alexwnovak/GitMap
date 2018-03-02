1b15b751a7e7a30105b16cc92079527d18a5f780
Alex Novak <alexwnovak@gmail.com>
2018-03-01 22:54:19 -0500

    MainViewModel now receives the IDialogService

--- GitMap/GitMap.ConfigurationUI.UnitTests/ViewModels/MainViewModelTests.cs
+++ GitMap/GitMap.ConfigurationUI.UnitTests/ViewModels/MainViewModelTests.cs
@@ -1,6 +1,28 @@
-﻿namespace GitMap.ConfigurationUI.UnitTests.ViewModels
+﻿using System;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using GitMap.ConfigurationUI.Services;
+using GitMap.ConfigurationUI.ViewModels;
+
+namespace GitMap.ConfigurationUI.UnitTests.ViewModels
 {
    public class MainViewModelTests
    {
+      [Fact]
+      public void Constructor_FactoryIsNull_ThrowsArgumentNullException()
+      {
+         Action constructor = () => new MainViewModel( null, Mock.Of<IDialogService>() );
+
+         constructor.Should().Throw<ArgumentNullException>();
+      }
+
+      [Fact]
+      public void Constructor_DialogServiceIsNull_ThrowsArgumentNullException()
+      {
+         Action constructor = () => new MainViewModel( Mock.Of<IEditorViewModelFactory>(), null );
+
+         constructor.Should().Throw<ArgumentNullException>();
+      }
    }
 }
--- GitMap/GitMap.ConfigurationUI/ViewModels/MainViewModel.cs
+++ GitMap/GitMap.ConfigurationUI/ViewModels/MainViewModel.cs
@@ -1,15 +1,20 @@
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GitMap.ConfigurationUI.Properties;
+using GitMap.ConfigurationUI.Services;
 using GitMap.Core;
 
 namespace GitMap.ConfigurationUI.ViewModels
 {
    public class MainViewModel : ViewModelBase
    {
+      private readonly IDialogService _dialogService;
+
       public ObservableCollection<EditorViewModel> EditorViewModels
       {
          get;
@@ -25,8 +30,15 @@ namespace GitMap.ConfigurationUI.ViewModels
          get;
       }
 
-      public MainViewModel( IEditorViewModelFactory editorViewModelFactory )
+      public MainViewModel( IEditorViewModelFactory editorViewModelFactory, IDialogService dialogService )
       {
+         if ( editorViewModelFactory == null )
+         {
+            throw new ArgumentNullException( nameof( editorViewModelFactory ) );
+         }
+
+         _dialogService = dialogService ?? throw new ArgumentNullException( nameof( dialogService ) );
+
          var commit = editorViewModelFactory.Create( WorkflowNames.CommitWorkflow, Resources.Commit );
          var rebase = editorViewModelFactory.Create( WorkflowNames.RebaseWorkflow, Resources.Rebase );
 
