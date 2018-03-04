23a9930df26591cbf7b7175b7debebd5a571ce23
Alex Novak <alexwnovak@gmail.com>
2018-03-03 22:00:01 -0500

    ConfigurationWriter can now write to the registry

--- GitMap/GitMap.Core/ConfigurationWriter.cs
+++ GitMap/GitMap.Core/ConfigurationWriter.cs
@@ -1,10 +1,17 @@
-﻿namespace GitMap.Core
+﻿using Microsoft.Win32;
+
+namespace GitMap.Core
 {
    public class ConfigurationWriter : IConfigurationWriter
    {
       public void Write( string workflowName, EditorConfiguration editorConfiguration )
       {
-         throw new System.NotImplementedException();
+         using ( var key = Registry.CurrentUser.CreateSubKey( @"SOFTWARE\GitMap" ) )
+         {
+            key.SetValue( $"{workflowName}IsEnabled", editorConfiguration.IsEnabled );
+            key.SetValue( $"{workflowName}FilePath", editorConfiguration.FilePath );
+            key.SetValue( $"{workflowName}Arguments", editorConfiguration.Arguments );
+         }
       }
    }
 }
