6068f7040a9b37a6450ed56d51bb68431be7a1d4
Alex Novak <alexwnovak@gmail.com>
2018-03-03 21:13:30 -0500

    +IConfigurationWriter interface and blank concrete

---
+++ GitMap/GitMap.Core/ConfigurationWriter.cs
@@ -0,0 +1,10 @@
+﻿namespace GitMap.Core
+{
+   public class ConfigurationWriter : IConfigurationWriter
+   {
+      public void Write( string workflowName, EditorConfiguration editorConfiguration )
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
