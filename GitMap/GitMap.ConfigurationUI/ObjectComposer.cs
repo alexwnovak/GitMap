13d8d01cfa9571f85ddeb86902ebd8daeaabe6bb
Alex Novak <alexwnovak@gmail.com>
2018-04-15 22:45:47 -0400

    +ObjectComposer blank class
    This will be the thing that wires up the DI stuff.

---
+++ GitMap/GitMap.ConfigurationUI/ObjectComposer.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace GitMap.ConfigurationUI
+{
+   public class ObjectComposer
+   {
+      private readonly ISimpleIoc _container;
+
+      public ObjectComposer( ISimpleIoc container )
+      {
+         _container = container;
+      }
+   }
+}
