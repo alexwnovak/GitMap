44b3b1403fa9f2ea2809dfe255390935ce3403d6
Alex Novak <alexwnovak@gmail.com>
2018-04-15 22:49:12 -0400

    Moving all DI stuff into the object composer

--- GitMap/GitMap.ConfigurationUI/ObjectComposer.cs
+++ GitMap/GitMap.ConfigurationUI/ObjectComposer.cs
@@ -1,14 +1,39 @@
-﻿using GalaSoft.MvvmLight.Ioc;
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight.Ioc;
+using GitMap.ConfigurationUI.Services;
+using GitMap.ConfigurationUI.ViewModels;
+using GitMap.Core;
+using Resx = GitMap.ConfigurationUI.Properties.Resources;
 
 namespace GitMap.ConfigurationUI
 {
    public class ObjectComposer
    {
-      private readonly ISimpleIoc _container;
+      public ISimpleIoc Container { get; }
 
       public ObjectComposer( ISimpleIoc container )
       {
-         _container = container;
+         Container = container;
+      }
+
+      public void Compose()
+      {
+         Container.Register<IConfigurationReader, ConfigurationReader>();
+         Container.Register<IConfigurationWriter, ConfigurationWriter>();
+         Container.Register<IFileBrowserService, FileBrowserService>();
+         Container.Register<IDialogService, DialogService>();
+         Container.Register( CreateEditorViewModels );
+      }
+
+      private IEnumerable<IEditorViewModel> CreateEditorViewModels()
+      {
+         var fileBrowserService = Container.GetInstance<IFileBrowserService>();
+
+         return new[]
+         {
+            new EditorViewModel( fileBrowserService, WorkflowNames.CommitWorkflow, Resx.Commit ),
+            new EditorViewModel( fileBrowserService, WorkflowNames.RebaseWorkflow, Resx.Rebase )
+         };
       }
    }
 }
