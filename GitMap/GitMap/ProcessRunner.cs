c89e73cce9eec9a7e6768769cc985e1d1c431538
Alex Novak <alexwnovak@gmail.com>
2019-02-21 20:50:28 -0500

    +Static Run method

--- GitMap/GitMap/ProcessRunner.cs
+++ GitMap/GitMap/ProcessRunner.cs
@@ -4,6 +4,16 @@ namespace GitMap
 {
    public class ProcessRunner : IProcessRunner
    {
+      public static int Run( string fileName, string arguments )
+      {
+         using ( var process = Process.Start( fileName, arguments ) )
+         {
+            process.WaitForExit();
+
+            return process.ExitCode;
+         }
+      }
+
       public int RunOld( string fileName, string arguments )
       {
          using ( var process = Process.Start( fileName, arguments ) )
